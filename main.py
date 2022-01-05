406019251b9d1e66835c83b6f0828a4aaad5a6c6
Alex <alex.gyumushyan@gmail.com>
2022-01-04 23:43:10 -0500

    added next generation function to calculate the next gen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame import surface
 from pygame.constants import WINDOWSIZECHANGED
+from pygame.cursors import arrow
 
 BLACK = [0, 0, 0]
 WHITE = [255, 255, 255]
@@ -8,10 +9,13 @@ WHITE = [255, 255, 255]
 WINDOW_HEIGHT = 500
 WINDOW_WIDTH = 500
 
-ARR = [[0] * 25] * 25
+ARR = []
+
+for i in range(25):
+    ARR.append([0] * 25)
 
 
-def drawGrid():
+def drawGridLines():
     blockSize = 20
 
     for x in range(0, WINDOW_WIDTH, blockSize):
@@ -20,6 +24,57 @@ def drawGrid():
             pygame.draw.rect(screen, WHITE, rect, 1)
 
     pygame.display.update()
+
+def drawGrid(grid):
+
+    for i in range(25):
+        for j in range(25):
+            rect = pygame.Rect(x * 20, y * 20, 19, 19)
+            if grid[i] [j] == 1:
+                pygame.draw.rect(screen, [200, 200, 200], rect)
+            else:
+                pygame.draw.rect(screen, [0,0,0], rect)
+
+    pygame.display.update()
+
+def nextGeneration(grid):
+    nextGen = []
+    for i in range(25):
+        nextGen.append([0] * 25)
+    
+    def countNeighbours(i, j):
+        counter = 0
+
+        for x in range(-1, 2):
+            for y in range(-1, 2):
+                if grid[i + x] [j + y] == 1:
+                    counter += 1
+
+
+        if grid[i] [j] == 1:
+            counter -= 1
+            print(f"cell ({i},{j}) has a neighbour count of {counter}")
+
+        return counter 
+    
+    for i in range(1,24):
+        for j in range(1,24):           
+            if i == 2 and j == 1:
+                print("here")
+            nOfNeighbour = countNeighbours(i, j)
+            
+            if grid[i][j] == 1 and (nOfNeighbour == 3 or nOfNeighbour == 2):
+                nextGen[i][j] = 1
+            elif  grid[i][j] == 0 and nOfNeighbour == 3:
+                nextGen[i][j] = 1
+            else:
+                if grid[i][j] == 1:
+                    nextGen[i][j] = 0                
+
+
+    return nextGen
+
+
 
 if __name__ == '__main__':
     pygame.init()
@@ -33,7 +88,7 @@ if __name__ == '__main__':
     gameOfLife = False
 
     while running:
-        drawGrid()
+        drawGridLines()
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -49,7 +104,7 @@ if __name__ == '__main__':
 
                     print(f"x: {x}        y: {y}")
 
-                    ARR[x][y] = 1
+                    ARR[x][y] = 1 # incorrect
 
                     rect = pygame.Rect(x * 20, y * 20, 19, 19)
                     pygame.draw.rect(screen, [200, 200, 200], rect)
@@ -57,6 +112,9 @@ if __name__ == '__main__':
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:                   
                     print("start algorithm")
+                    ARR = nextGeneration(ARR)
+                    drawGrid(ARR)
+
                     gameOfLife = True
                 if event.key == pygame.K_BACKSPACE:
                     print("backspace, end game")
