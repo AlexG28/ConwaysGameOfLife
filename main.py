6fd1eaf55b465c1ffaafb0a9ef0486b2b2563090
Alex <alex.gyumushyan@gmail.com>
2022-01-04 22:17:14 -0500

    added grid, array and keyboard controls

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame import surface
 from pygame.constants import WINDOWSIZECHANGED
 
 BLACK = [0, 0, 0]
@@ -6,6 +7,8 @@ WHITE = [255, 255, 255]
 
 WINDOW_HEIGHT = 500
 WINDOW_WIDTH = 500
+
+ARR = [[0] * 25] * 25
 
 
 def drawGrid():
@@ -15,6 +18,7 @@ def drawGrid():
         for y in range(0, WINDOW_HEIGHT, blockSize):
             rect = pygame.Rect(x, y, blockSize, blockSize)
             pygame.draw.rect(screen, WHITE, rect, 1)
+
     pygame.display.update()
 
 if __name__ == '__main__':
@@ -26,6 +30,7 @@ if __name__ == '__main__':
     pygame.display.update()
 
     running = True
+    gameOfLife = False
 
     while running:
         drawGrid()
@@ -33,5 +38,29 @@ if __name__ == '__main__':
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_press = pygame.mouse.get_pressed()
+
+                if mouse_press[0]:                    
+                    position = pygame.mouse.get_pos()
+
+                    x = int(position[0] / 20)
+                    y = int(position[1] / 20)
+
+                    print(f"x: {x}        y: {y}")
+
+                    ARR[x][y] = 1
+
+                    rect = pygame.Rect(x * 20, y * 20, 19, 19)
+                    pygame.draw.rect(screen, [200, 200, 200], rect)
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:                   
+                    print("start algorithm")
+                    gameOfLife = True
+                if event.key == pygame.K_BACKSPACE:
+                    print("backspace, end game")
+                    gameOfLife = False
+
 
     pygame.quit()
